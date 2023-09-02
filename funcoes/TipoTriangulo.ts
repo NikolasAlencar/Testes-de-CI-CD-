0b1139586d085ca81bad784fe78da749deb2ecc0
Arthur <siqueirarthur70@gmail.com>
2023-09-01 21:19:54 -0300

    funcao tipo triangulo

---
+++ funcoes/TipoTriangulo.ts
@@ -0,0 +1,9 @@
+import { ehTriangulo } from "./EhTriangulo"
+
+export const tipoTriangulo = (A: number, B: number, C: number) => {
+    if(ehTriangulo(A, B, C)){
+
+    }else{
+        console.log('Não é um triângulo!');
+    }
+}
